50d482f782f481eb3fb9cb214a24c9782ad4fb81
Bhumika <bpatel63@hawk.iit.edu>
2018-12-05 02:29:59 -0600

    updating signup.html
    adding content for thepop up signup form for the ISUD Events page

--- js/signup.js
+++ js/signup.js
@@ -1,1 +1,12 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
 
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+function confirmSubmit(){
+  var fullname = document.getElementById("firstname");
+  alert("Thanks " + firstname.value + " for joining ISUD XII World Congress!");
+}
